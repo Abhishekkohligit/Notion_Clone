0db7aed328c98915ac939e7c1a85c2e3d2bb211b
Abhishek Kohli <ab02110211@gmail.com>
2023-10-28 08:01:19 +0100

    add LAnding page. & connect to covex, not auth yet

--- app/(marketing)/_components/heroes.tsx
+++ app/(marketing)/_components/heroes.tsx
@@ -3,11 +3,17 @@ import Image from "next/image";
 export const Heroes = () => {
 	return (
 		<div className="flex flex-col items-center justify-center max-w-5xl">
-			<div className="flex items-center">
+			<div className="flex items-center ">
 				<div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:w-[400px] md:h-[400px]">
 					<Image
 						src="/documents.png"
-						className="object-containt"
+						className="object-contain dark:hidden"
+						fill
+						alt="Document"
+					/>
+					<Image
+						src="/documents-dark.png"
+						className="object-contain hidden dark:block"
 						fill
 						alt="Document"
 					/>
@@ -16,7 +22,13 @@ export const Heroes = () => {
 				<div className="relative w-[400px] h-[400px] hidden md:block">
 					<Image
 						src="/reading.png"
-						className="object-containt"
+						className="object-contain dark:hidden"
+						fill
+						alt="Reading"
+					/>
+					<Image
+						src="/reading-dark.png"
+						className="object-contain hidden dark:block	"
 						fill
 						alt="Reading"
 					/>
---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+	providers: [
+		{
+			domain: "https://optimal-roughy-89.clerk.accounts.dev",
+			applicationID: "convex",
+		},
+	],
+};
