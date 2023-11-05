2668af0c1fb0d07c096e2b2c55897ba366699c24
Abhishek Kohli <ab02110211@gmail.com>
2023-11-05 12:03:29 +0000

    fixed menu icon err & added the colaps on sidebar

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -5,6 +5,7 @@ import { usePathname } from "next/navigation";
 import { useRef, ElementRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
 import { MenuIcon } from "lucide-react";
+// import { log } from "console";
 
 export const Navigation = () => {
 	const isMobile = useMediaQuery("(max-width:768px)");
@@ -50,8 +51,42 @@ export const Navigation = () => {
 		document.removeEventListener("mousemove", handleMouseMove);
 	};
 
+	const resetWidth = () => {
+		if (sidebarRef.current && navbarRef.current) {
+			setIsCollapsed(false);
+			setIsResetting(true);
+
+			sidebarRef.current.style.width = isMobile ? "100%" : "240px";
+			navbarRef.current.style.setProperty(
+				"width",
+				isMobile ? "0" : "calc(100%-240px)"
+			);
+
+			navbarRef.current.style.setProperty("left", isMobile ? "100%" : "240px");
+		}
+
+		setTimeout(() => setIsResetting(false), 300);
+	};
+
+	const collapse = () => {
+		if (sidebarRef.current && navbarRef.current) {
+			setIsCollapsed(true);
+			setIsResetting(true);
+
+			sidebarRef.current.style.width = "0";
+			navbarRef.current.style.setProperty("width", "100%");
+			navbarRef.current.style.setProperty("left", "0");
+			setTimeout(() => setIsResetting(false), 300);
+		}
+	};
+
+	// console.log(isCollapsed);
+	// console.log(isMobile);
+	console.log(navbarRef, "navbarRef");
+	console.log(sidebarRef, "sidebarref");
+
 	return (
-		<>
+		<div className="flex relative">
 			<aside
 				className={cn(
 					"group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]",
@@ -62,6 +97,7 @@ export const Navigation = () => {
 			>
 				<div
 					role="button"
+					onClick={collapse}
 					className={cn(
 						"h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition",
 						isMobile && "opacity-100"
@@ -79,7 +115,7 @@ export const Navigation = () => {
 
 				<div
 					onMouseDown={handleMouseDown}
-					onClick={handleMouseUp}
+					onClick={resetWidth}
 					className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 top-0 right-0"
 				/>
 			</aside>
@@ -89,15 +125,19 @@ export const Navigation = () => {
 				className={cn(
 					"absolute top-0 z-[99999] left-60 w-[calc(100%-240px)]",
 					isResetting && "transition-all ease-in-out duration-300",
-					isMobile && "left-0 w-full"
+					isMobile && "left-2 w-full"
 				)}
 			>
-				<nav className="bg-transparent w-full py-2 px-3">
+				<nav className="bg-transparent w-full">
 					{isCollapsed && (
-						<MenuIcon className="h-6 w-6 text-muted-foreground" role="button" />
+						<MenuIcon
+							onClick={resetWidth}
+							className="h-6 w-6 text-muted-foreground bg-yellow-500"
+							role="button"
+						/>
 					)}
 				</nav>
 			</div>
-		</>
+		</div>
 	);
 };
